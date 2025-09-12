841e861d94084e7d5132e126178309d6b33e66f8
merajfaishal255 <147262864+merajfaishal255@users.noreply.github.com>
2025-09-12 18:19:55 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,11 +1,1 @@
-import streamlit as st
-import joblib  
-import numpy as np
-import pandas as pd
 
-
-st.title("❤️ Heart Stroke Risk Predictor        (Faishal Meraj)")
-st.markdown("**Important:** This is an educational demo — not medical advice.")
-
-# Load model
-model = joblib.load("stroke_model.pkl")  # the pipeline saved by train_model.py
